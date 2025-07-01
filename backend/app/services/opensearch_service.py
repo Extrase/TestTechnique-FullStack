d717096820fae6b2d6a342c30afed1bdd06b3122
Diavolo <manon2245@outlook.fr>
2025-07-01 23:52:05 +0200

    OpenSearch initalise et utilisation d l\'environnement avec Pydantic :)

--- backend/app/services/opensearch_service.py
+++ backend/app/services/opensearch_service.py
@@ -1,42 +1,67 @@
 from opensearchpy import OpenSearch
+from datetime import datetime
+from ..config import Settings
 
-host = 'localhost'
-port = 9200
-
-client = OpenSearch(
-    hosts = [{'host': host, 'port': port}],
-    http_compress = True,
-    use_ssl = False,
-    verify_certs = False,
-    ssl_assert_hostname = False,
-    ssl_show_warn = False
-)
-
-document = {
-  'title': 'Moneyball',
-  'director': 'Bennett Miller',
-  'year': '2011'
-}
-
-response = client.index(
-    index = 'python-test-index',
-    body = document,
-    id = '1',
-    refresh = True
-)
-
-q = 'miller'
-query = {
-  'size': 5,
-  'query': {
-    'multi_match': {
-      'query': q,
-      'fields': ['title^2', 'director']
+class OpenSearchService:
+    def __init__(self):
+        settings = Settings()
+        host = settings.opensearch_host # recupere la variable d'environnement, tout en mettant une valeur par defaut en cas d'erreur
+        port = settings.opensearch_port # identique
+        self.client = OpenSearch(
+            hosts = [{'host': host, 'port': port}],
+            http_compress = True, 
+            use_ssl = False,
+            verify_certs = False,
+            ssl_assert_hostname = False,
+            ssl_show_warn = False
+        )
+    def _get_index_name(self, timestamp: str) -> str:
+        """Génère le nom d'index basé sur la date du timestamp"""
+        try:
+            dt = datetime.fromisoformat(timestamp) # parse le timestamp
+            dt_iso = dt.date() # extrait juste la date
+            log = dt_iso.strftime("%Y.%m.%d") # Format YYYY.MM.DD
+            return f"logs-{log}" # préfixe "logs-"
+        except ValueError:
+            raise ValueError("Invalid timestamp format, it must be ISO 8601")
+        
+    def index_log(self, log_data: dict) -> dict:
+        """Indexe un log dans OpenSearch et retourne l'ID généré"""
+        timestamp = log_data["timestamp"] # a ce stade timestamp = "2025-07-01T14:30:00"
+        index_name = self._get_index_name(timestamp) # index_name = "logs-2025.07.01"
+        response = self.client.index(
+            index=index_name, # "logs-2025.07.01"
+            body=log_data, # les données de log
+            refresh = True # ]our voir le résultat immédiatement
+        )
+        generated_id = response["_id"] # OpenSearch donne un ID dans la réponse
+        final_log = log_data.copy() # pour ne opas modifier l'original
+        final_log["id"] = generated_id
+        return final_log
+        
+    def search_logs(self, q: str = None, level: str = None, service: str = None) -> list:
+        """Recherche les logs avec filtres optionnels"""
+        query = {
+        "size": 100,
+        "sort": [{"timestamp": {"order": "desc"}}],
+        "query": {
+            "bool": {
+                "must": [],    # conditions obligatoires
+                "filter": []   # les filtres
+            }
+        }
     }
-  }
-}
-
-response = client.search(
-    body = query,
-    index = 'python-test-index'
-)
+    # ajouter les conditions optionnelles selon les paramètres
+        if q: 
+            query["query"]["bool"]["must"].append({"match": {"message": q}}) # match cherche une string dans 
+        if level:
+            query["query"]["bool"]["filter"].append({"term": {"level": level}}) #term demande "est-ce que ce level est present ?"
+        if service:
+            query["query"]["bool"]["filter"].append({"term": {"service": service}})
+        response = self.client.search(body=query, index="logs-*") # tout index commencqnt par "logs-")
+        search_ids = []
+        for hit in response["hits"]["hits"]: # on avance dans chque iteration de response.hits.hits
+            tmp = hit["_source"].copy() # copier pour eviter de ;odifier le conteu originel
+            tmp["id"] = hit["_id"] # j'qjoute l'id dans le resultat final
+            search_ids.append(tmp)
+        return search_ids # je return les logs dans l'ordre decroissant de la requete initiale
